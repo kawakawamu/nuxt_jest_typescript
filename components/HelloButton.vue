a3242cf1e94989a69da6a59da6be31b3abcb7d99
河村 泰佑 <kata.kawa813@gmail.com>
2021-08-23 22:32:59 +0900

    add

---
+++ components/HelloButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div><b-btn>Button</b-btn></div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Button extends Vue {}
+</script>
