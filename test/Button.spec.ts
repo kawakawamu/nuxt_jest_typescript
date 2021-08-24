3c9d4a6902ab0a727852998250d70d60880a88f9
河村 泰佑 <kata.kawa813@gmail.com>
2021-08-25 00:33:30 +0900

    add

--- test/Button.spec.ts
+++ test/Button.spec.ts
@@ -1,2 +1,9 @@
 import { mount } from '@vue/test-utils'
 import Button from '@/components/HelloButton.vue'
+
+describe('HelloButton.vue', () => {
+  it('snapshot', () => {
+    const wrapper = mount(Button)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
