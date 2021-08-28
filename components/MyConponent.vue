a8c271d1bd5f62f8b1ba2ee33290cdd8ef8c1125
河村 泰佑 <kata.kawa813@gmail.com>
2021-08-28 16:27:56 +0900

    add

--- components/MyConponent.vue
+++ components/MyConponent.vue
@@ -3,3 +3,15 @@
     <p>{{ text }}</p>
   </div>
 </template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class MyComponent extends Vue {
+  @Prop() private text!: string
+
+  onClickBtn() {
+    console.log('clicked')
+  }
+}
+</script>
