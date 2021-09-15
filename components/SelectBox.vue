20df16a310b5226023173ba6c824165a469770b1
河村 泰佑 <kata.kawa813@gmail.com>
2021-09-15 21:35:43 +0900

    セレクトボックスの追加

--- components/SelectBox.vue
+++ components/SelectBox.vue
@@ -1,17 +1,58 @@
 <template>
-  <select v-model="selectedKey" v-on:change="selected">
-    <option v-for="(value, key) in items">{{ key }}</option>
-  </select>
+  <div>
+    <div>セレクトボックス</div>
+    <select v-model="selectedKey" v-on:change="selected">
+      <option v-for="(value, key) in food_items" :key="key">{{ key }}</option>
+    </select>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
+// 二次元配列の型定義
+interface FoodItem {
+  id: number
+  food_name: string
+}
+interface FoodItems {
+  [key: string]: FoodItem[]
+}
+
 @Component
 export default class SelectBox extends Vue {
   selectedKey: string = ''
-  // items: {
-  //   野菜: [{ name: '白菜' }, { name: '人蔘' }]
-  //   果物: [{ name: '苺' }, { name: '林檎' }]
-  // }
+  selectedItem: [] = []
+
+  // 二次元配列
+  food_items: FoodItems = {
+    fruit: [
+      {
+        id: 0,
+        food_name: '苺'
+      },
+      {
+        id: 1,
+        food_name: '林檎'
+      }
+    ],
+    vegetable: [
+      {
+        id: 2,
+        food_name: '白菜'
+      },
+      {
+        id: 3,
+        food_name: 'なす'
+      },
+      {
+        id: 4,
+        food_name: '大根'
+      }
+    ]
+  }
+
+  selected() {
+    // this.selectedItem = this.food_items[this.selectedKey]
+  }
 }
 </script>
